5d6c9b09d0c780c358bedc1bf0722b9576e648f9
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-09-06 23:55:47 +0100

    - fixed npm start deployment bug

--- backend/server.ts
+++ backend/server.ts
@@ -30,11 +30,11 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 	app.get("*", (req, res) => {
 		res.sendFile(
-			path.resolve(__dirname, "..", "frontend", "dist", "index.html")
+			path.resolve(__dirname, "..", "..", "frontend", "dist", "index.html")
 		);
 	});
 }
