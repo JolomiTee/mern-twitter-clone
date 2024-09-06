a54849cc9abb7279299bde2822920fa1e621f848
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-09-06 23:10:00 +0100

    modified:   frontend/index.html

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import path from "path";
 import express, { Express } from "express";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
@@ -27,6 +28,16 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, "..", "frontend", "dist", "index.html")
+		);
+	});
+}
 
 app.listen(8000, () => {
 	console.log(`Server is running on port ${PORT}`);
